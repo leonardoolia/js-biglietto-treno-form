97ddcdad4ec6b3dc431a39f10b2032e684490066
leonardoolia <leonardooliaa@gmail.com>
2023-12-23 15:11:37 +0100

    got dom elements in js

---
+++ js/script.js
@@ -0,0 +1,26 @@
+//? 1. Recuperare gli elementi dal DOM
+
+// Elementi del form
+const nameField = document.getElementById('name');
+const kmsField = document.getElementById('kms');
+const ageField = document.getElementById('age');
+const ticketForm = document.getElementById('ticket-form');
+
+// Bottoni del form
+const confirmButton = document.getElementById('confirm-button');
+const resetButton = document.getElementById('reset-button');
+
+// Bottone del biglietto
+const ticketButton = document.getElementById('ticket-button');
+
+// Elementi del biglietto
+const ticketSection = document.getElementById('ticket-section');
+const passengerName = document.getElementById('passenger-name');
+const ticketRate = document.getElementById('rate');
+const cabNumber = document.getElementById('cab');
+const cpCodeNumber = document.getElementById('cp-code');
+const price = document.getElementById('price');
+
+
+
+
